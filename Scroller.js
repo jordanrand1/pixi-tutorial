4f5bbcfb1e15149e10c5e699bb4f2dba2bf18099
jordanrand1 <jorrand@gmail.com>
2019-01-21 14:15:08 -0700

    viewport scroll

--- Scroller.js
+++ Scroller.js
@@ -4,9 +4,21 @@ function Scroller(stage) {
 
     this.mid = new Mid();
     stage.addChild(this.mid);
+
+    this.viewportX = 0;
 }
 
 Scroller.prototype.setViewportX = function(viewportX) {
+    this.viewportX = viewportX; 
     this.far.setViewportX(viewportX);
     this.mid.setViewportX(viewportX);
+}
+
+Scroller.prototype.getViewportX = function() {
+    return this.viewportX;
+}
+
+Scroller.prototype.moveViewportXBy = function(units) {
+    var newViewportX = this.viewportX + units;
+    this.setViewportX(newViewportX);
 }
