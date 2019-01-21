85a404de34fdb626e254de33f64b706d517eab98
jordanrand1 <jorrand@gmail.com>
2019-01-21 13:41:58 -0700

    update function

--- Far.js
+++ Far.js
@@ -9,3 +9,7 @@ function Far() {
 }
 
 Far.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
+
+Far.prototype.update = function() {
+    this.tilePosition.x -= 0.128;
+}
--- Mid.js
+++ Mid.js
@@ -3,9 +3,13 @@ function Mid() {
     PIXI.extras.TilingSprite.call(this, texture, 512, 256);
 
     this.position.x = 0;
-    this.position.y = 0;
+    this.position.y = 128;
     this.tilePosition.x = 0;
     this.tilePosition.y = 0;
 }
 
 Mid.prototype = Object.create(PIXI.extras.TilingSprite.prototype);
+
+Mid.prototype.update = function() {
+    this.tilePosition.x -= 0.64;
+}
